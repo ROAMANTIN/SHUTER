7042dcca763669877b37153f8dfb0b94c56fd6d1
ROAMANTIN <99786029+ROAMANTIN@users.noreply.github.com>
2024-04-06 11:11:07 +0300

    Update save.py

--- program/save.py
+++ program/save.py
@@ -1,1 +1,15 @@
+class SavePoint:
+  def __init__(self):
+    #Инициализация точек сохранения
 
+  def SavePointSaving(self):
+    #Сохранение точки сохранения
+
+  def ChangeSavePoint(self):
+    #Изменение сохранённой точки сохранения
+
+  def ActivationSave(self):
+    #Активация сохранения
+
+  def DeleteLastSave(self):
+    #Удаление последнего сохранения
