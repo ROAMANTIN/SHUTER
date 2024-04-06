9d122a4b9b41a8e249cb0db6c9eff7dc1f4a3f4c
ROAMANTIN <99786029+ROAMANTIN@users.noreply.github.com>
2024-04-06 11:09:57 +0300

    Update shoot.py

--- program/shoot.py
+++ program/shoot.py
@@ -1,1 +1,22 @@
+class Cursor:
+  def __init__(self):
+    #Курсор
 
+  def StartCursor(self):
+    #Начало курсора
+
+  def UpdateCursor(self):
+    #Обновление курсора
+
+class Shooting:
+  def __init__(self):
+    #Стрельба
+
+  def StartShoot(self):
+    #Начало стрельбы
+
+  def ChangeShoot(self):
+    #Изменение стрельбы
+
+  def ShowVector(self):
+    #Показывание вектора стрельбы
