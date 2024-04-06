413f78dce8f12a84307c74c908a01fdd5856b4f3
ROAMANTIN <99786029+ROAMANTIN@users.noreply.github.com>
2024-04-06 10:22:07 +0300

    Update capture.py

--- program/capture.py
+++ program/capture.py
@@ -1,1 +1,12 @@
+class CaptureTerritory:
+  def __init__(self):
+    #Захват территории
 
+  def StartCapture(self):
+    #Начало захвата территории
+
+  def ChooseBonus(self):
+    #Выбор бонуса за захват
+
+  def DestroyingWorld(self):
+    #Разрушение мира и окружения
